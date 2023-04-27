99bd8f01e8e7335c43ba6cc6d0d13e28941e4087
lokmvne <lokmankerboua@gmail.com>
2023-04-28 00:43:28 +0100

    host

--- database.js
+++ database.js
@@ -1,11 +1,11 @@
 const {Client} = require('pg');
 
 const client = new Client({
-    host: 'localhost',
-    user: "postgres",
+    host: 'postgres://net_w1yi_user:1Tr1v7mbn2sRKiyCtKwFQJvYi5jQbBYr@dpg-ch5gfabh4hsvr4co5m30-a/net_w1yi',
+    user: "net_w1yi_user",
     port: 5432,
-    password: 'Lokmano9',
-    database: 'net'
+    password: '1Tr1v7mbn2sRKiyCtKwFQJvYi5jQbBYr',
+    database: 'net_w1yi'
 })
 
 module.exports = client
