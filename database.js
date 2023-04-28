6357e4ba7025065110b7f3e9d77aedc536eb7d19
lokmvne <lokmankerboua@gmail.com>
2023-04-28 20:52:35 +0100

    hosting

--- database.js
+++ database.js
@@ -1,5 +1,12 @@
 const {Client} = require('pg');
-const connectString = "postgres://net_w1yi_user:1Tr1v7mbn2sRKiyCtKwFQJvYi5jQbBYr@dpg-ch5gfabh4hsvr4co5m30-a.oregon-postgres.render.com/net_w1yi"
+// const connectString = new Client("postgres://net_w1yi_user:1Tr1v7mbn2sRKiyCtKwFQJvYi5jQbBYr@dpg-ch5gfabh4hsvr4co5m30-a.oregon-postgres.render.com/net_w1yi?ssl:true")
+
+const connectString = new Client({
+    connectionString: "postgres://net_w1yi_user:1Tr1v7mbn2sRKiyCtKwFQJvYi5jQbBYr@dpg-ch5gfabh4hsvr4co5m30-a.oregon-postgres.render.com/net_w1yi",
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
 
 const client = new Client(connectString)
 
